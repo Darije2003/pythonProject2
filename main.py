1be7f550b8970c13435e46deeeaf18cc436af826
Darko <darko.stupar.03@gmail.com>
2021-08-18 16:56:48 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+print('Unesite dva broja za racunanje')
+
+a=int(input('a='))
+b=int(input('b='))
+
+print('↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ')
+
+q=a+b
+w=a-b
+o=b-a
+e=a*b
+r=a/b
+t=b/a
+
+print('Zbir:',q)
+print('Razlika a-b:',w)
+print('Razlika b-a:',o)
+print('Proizvod:',e)
+print('Kolicnik a/b:',r)
+print('Kolicnik b/a:',t)
+print('--------------------------------------------------------')
